41283bb124ae9ff4f29cffa914b62616dd3dd46c
dwilliams73 <devongrad2022@gmail.com>
2026-02-04 15:42:33 -0600

    TEST FOR HENRY

---
+++ Assets/Scripts/testingforNerd.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testingforNerd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //it totally works and im a genius pretty much
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
